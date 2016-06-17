63b1cbf5ff4ba30caed546e409ecfb834c8d2525
Marko Zabcic <gm.marko@gmail.com>
2016-06-17 16:09:04 +0200

    Remove some console log

--- app/scripts/views/modal.js
+++ app/scripts/views/modal.js
@@ -33,7 +33,6 @@ module.exports = Core.Modal = View.extend({
   },
 
   open: function(){
-    console.log('OPEEEEEEEEEEEN');
     if(this.is_open){ return this; }
     this.render().insert().$el.modal({});
     this.trigger('open');
