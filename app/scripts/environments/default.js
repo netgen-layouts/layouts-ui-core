e5d4c7d38728c9265f87736ae96afed0acca6022
Edi Modrić <edi.modric@gmail.com>
2018-06-20 21:01:09 +0200

    Make the app and api URLs as variables

--- app/scripts/environments/default.js
+++ app/scripts/environments/default.js
@@ -6,17 +6,21 @@ function normalize_path(path) {
 
 module.exports =  {
   bm_base_path: '/bm',
+  bm_base_app_path: '/app/',
+  bm_base_api_path: '/api/v1/',
+
   cb_base_path: '/cb',
+  cb_base_api_path: '/api/v1/',
 
   bm_api_url: function(path) {
-    return normalize_path(this.bm_base_path + '/api/v1/' + path);
+    return normalize_path(this.bm_base_path + this.bm_base_api_path + path);
   },
 
   bm_app_url: function(path) {
-    return normalize_path(this.bm_base_path + '/app/'+path);
+    return normalize_path(this.bm_base_path + this.bm_base_app_path + path);
   },
 
   cb_api_url: function(path) {
-    return normalize_path(this.cb_base_path + '/api/v1/'+path);
+    return normalize_path(this.cb_base_path + this.cb_base_api_path + path);
   }
 };
