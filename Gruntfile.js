bece1a3489102de4861e63f025f17a45fb87b9bc
Edi Modrić <edi.modric@gmail.com>
2018-07-12 16:39:43 +0200

    Restore build script as it is needed for templates.js

---
+++ Gruntfile.js
@@ -0,0 +1,77 @@
+'use strict';
+
+// Override Handlebars default name lookup
+var Handlebars = require('handlebars/lib/index');
+var JavaScriptCompiler = Handlebars.JavaScriptCompiler;
+
+var helpers = require('./app/scripts/helpers');
+
+var known_helpers = {};
+for (var k in helpers) {
+  known_helpers[k] = true;
+}
+
+JavaScriptCompiler.prototype.nameLookup = function(parent, name /* , type*/ ) {
+  return "Handlebars.r(" + parent + ",'" + name + "')";
+};
+
+module.exports = function(grunt) {
+  // load all grunt tasks
+  require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+
+  var config = {
+    app: 'app'
+  };
+
+  grunt.initConfig({
+    config: config,
+
+    watch: {
+      handlebars: {
+        files: ['<%= config.app %>/templates/**/*.hbs', 'tests/templates/**/*.hbs'],
+        tasks: ['handlebars']
+      }
+    },
+
+    handlebars: {
+      compile: {
+        files: {
+          '<%= config.app %>/scripts/templates.js': '<%= config.app %>/templates/**/*.hbs'
+        },
+        options: {
+          compilerOptions: {
+            knownHelpers: known_helpers,
+            knownHelpersOnly: true
+          },
+          commonjs: true,
+          wrapped: true,
+          processPartialName: function(filename) {
+            return filename
+              .replace(/^app\/templates\//, '')
+              .replace(/_(\w+)\.hbs$/, '$1');
+          },
+          processName: function(filename) {
+            return filename
+              .replace(/^app\/templates\//, '')
+              .replace(/\.hbs$/, '');
+          }
+        }
+      }
+    },
+  });
+
+  grunt.registerTask('server', function() {
+    grunt.task.run([
+      'fast_build',
+      'watch'
+    ]);
+  });
+
+  grunt.registerTask('fast_build', function() {
+    grunt.task.run([
+      'handlebars'
+    ]);
+  });
+
+  grunt.registerTask('default', ['server']);
+};
