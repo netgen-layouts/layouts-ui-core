28b260fd646a9ac0344c684e9a051738c4e48cc8
Edi Modrić <edi.modric@gmail.com>
2018-07-13 09:47:18 +0200

    Remove paginator and pages selector templates, moved to content browser

--- app/scripts/helpers.js
+++ app/scripts/helpers.js
@@ -10,10 +10,6 @@ var safe = function(){
   return TagHelper.safe.apply(this, arguments);
 };
 
-var render_partial = function(template, context){
-  return TagHelper.render_partial.call(this, template, context);
-};
-
 module.exports = Core.handlebars_helpers = {
 
   t: function(key){
@@ -158,82 +154,6 @@ module.exports = Core.handlebars_helpers = {
     number = number || 300;
     return number / 1000;
   },
-
-  /**
-     * TODO !
-     *
-     * @method paginate
-     * @param  {String} id
-     * @param  {Object} options
-     * @return {String}
-     */
-  paginate: function(id, options){
-    var view = options.hash.context ? options.hash.context.view : this.view;
-    var view_pager = view.pagers[id];
-    if(!view_pager.pager){return '';}
-
-    var context = view_pager.pager;
-    context.id = id;
-    if(options.hash.auto_hide){
-      context.show = context.total_pages > 1;
-    }else{
-      context.show = true;
-    }
-
-    context.pages =  view_pager.pager.render();
-    context.prev_disabled = !context.prev;
-    context.next_disabled = !context.next;
-
-    return safe(render_partial('paginator', context));
-  },
-
-
-  /**
-   * Used as handlebars function. For existing view.pager creates dropdown to choose number of collection
-   * items displayed per page. On item select triggers collection.fetch event to update pagination
-   * (number of pages, items per page, ...)
-   * @method pages_selector
-   * @param  {String} id
-   * @return {String}
-   */
-  pages_selector: function(id){
-    console.log(this);
-    var view = this.view;
-    var view_pager = view.pagers[id];
-    if(!view_pager.pager){return '';}
-    var collection = view_pager.items;
-    var context = {
-      id: _.uniqueId('pages_selector'),
-      limit: collection.children_limit,
-      collection: [
-        {id: 5, name: 5},
-        {id: 10, name: 10},
-        {id: 25, name: 25},
-        //{id: 50, name: 50}
-      ]
-    };
-
-    _.defer(function(){
-      Core.$('#'+context.id).on('change', function(){
-        var data  = {
-          page: 1,
-          limit: Core.$(this).val()
-        };
-
-        if(view.name === 'search' && !view.collection.path.length){
-          collection.search_data({
-            data: _.extend(data, view.tabs.search_params())
-          });
-        }else{
-          collection.fetch_list({
-            data: data
-          });
-        }
-      });
-    });
-
-    return safe(render_partial('pages_selector', context));
-  }, // END OF pages_selector (function)
 
   get: function(model, property_name){
     return model.get(property_name);
